43fbd3ce6eb520c9bf7ffeb38a155cac1f2072bf
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-07-14 14:38:54 +0300

    Date optimized

--- src/App.js
+++ src/App.js
@@ -14,25 +14,27 @@ function App() {
       amount: 294.67,
       date: new Date(2021, 5, 19),
     },
-    { id: "e3", title: "new tv", amount: 364.2, date: new Date(2021, 10, 15) },
-    { id: "e4", title: "new desk", amount: 450, date: new Date(2021, 6, 4) },
+    {
+      id: "e3",
+      title: "new tv",
+      amount: 364.2,
+      date: new Date(2021, 10, 15),
+    },
+    {
+      id: "e4",
+      title: "new desk",
+      amount: 450,
+      date: new Date(2021, 6, 4),
+    },
   ];
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        expense={expenses[0]}
-      />
-      <ExpenseItem
-        expense={expenses[1]}
-      />
-      <ExpenseItem
-        expense={expenses[2]}
-      />
-      <ExpenseItem
-        expense={expenses[3]}
-      />
+      <ExpenseItem expense={expenses[0]} />
+      <ExpenseItem expense={expenses[1]} />
+      <ExpenseItem expense={expenses[2]} />
+      <ExpenseItem expense={expenses[3]} />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.expense.date.toLocaleString("en-US", { month: "long" });
+  const day = props.expense.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.expense.date.getFullYear();
 
   return (
     <div className="expense-item">
-      <div>{props.expense.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.expense.title}</h2>
         <div className="expense-item__price">${props.expense.amount}</div>
