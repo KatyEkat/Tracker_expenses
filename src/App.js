c6494bdb150a642df56572073b54cb2b253479bb
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-07-14 14:08:31 +0300

    updating props and changed the writing

--- src/App.js
+++ src/App.js
@@ -22,24 +22,16 @@ function App() {
     <div>
       <h2>Let's get started!</h2>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        expense={expenses[0]}
       />
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        expense={expenses[1]}
       />
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        expense={expenses[2]}
       />
       <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
+        expense={expenses[3]}
       />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,10 +4,10 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>{props.expense.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2>{props.expense.title}</h2>
+        <div className="expense-item__price">${props.expense.amount}</div>
       </div>
     </div>
   );
